baeeeed84b71a0405bacb89fb59aa6a9f46e5ffd
itlgl <itlgl@outlook.com>
2018-08-01 23:09:59 +0800

    :sparkles:仿照IM的例子，将sdk单独放在一个module中，删除之前app添加的jar包和so库

---
+++ tencent_im_sdk/src/main/java/com/itlgl/tencent/qcloud/Constants.java
@@ -0,0 +1,12 @@
+package com.itlgl.tencent.qcloud;
+
+public interface Constants {
+    /**
+     * 腾讯IM分配的Account Type
+     */
+    public static final int ACCOUNT_TYPE = 32703;
+    /**
+     * sdk appid 由腾讯分配
+     */
+    public static final int SDK_APPID = 1400096764;
+}
